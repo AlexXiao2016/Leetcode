dd9d59c417deabb572d84174169d01dc0cec4301
Ping Xiao <pxiao@apple.com>
2016-09-20 21:50:28 -0700

    Rotate Image

---
+++ src/Array/RotateImage.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class RotateImage {
+	public void rotate(int[][] matrix) {
+		if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+			return;
+		}
+		
+		int n = matrix.length;
+		int temp;
+		
+		int k = n;
+		// when i reach n/2, the rotate process will be done
+		for(int i = 0; i < n / 2; i++){
+			for(int j = i; j < k - 1; j++, k--){
+				temp = matrix[i][j];
+				matrix[i][j] = matrix[n - j - 1][i];
+				matrix[n - i - 1][i] = matrix[n - i - 1][n - j - 1];
+				matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+				matrix[j][n - i - 1] = temp;
+			}
+		}
+	}
+}
