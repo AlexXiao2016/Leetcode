7dc3ca46dc78fa899c340183cbf3e392a3f3be23
Ping Xiao <pxiao@apple.com>
2016-09-22 00:41:37 -0700

    Is Subsequence

---
+++ src/String/IsSubsequence.java
@@ -0,0 +1,28 @@
+package String;
+
+public class IsSubsequence {
+
+	public boolean isSubsequence(String s, String t) {
+		if(s == null || t == null){
+			return false;
+		}
+		
+		int sLen = s.length();
+		int tLen = t.length();
+		if(sLen > tLen){
+			return false;
+		}
+		
+		int i = 0, j = 0;
+		while(i < sLen && j < tLen){
+			if(s.charAt(i) == t.charAt(j++)){
+				i++;
+			}
+		}
+		
+		if(sLen == i){
+			return true;
+		}
+		return false;
+	}
+}
