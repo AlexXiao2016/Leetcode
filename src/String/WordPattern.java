e396eafaf5bdfa7f39eb26e2d7b66f5b05d26d6d
AlexXiao2016 <xpusjobs@gmail.com>
2016-09-26 02:45:24 +0800

    Word Pattern

---
+++ src/String/WordPattern.java
@@ -0,0 +1,33 @@
+package String;
+
+import java.util.HashMap;
+
+public class WordPattern {
+	 public boolean wordPattern(String pattern, String str) {
+		 HashMap<Character, String> patternMap = new HashMap<>();
+		 HashMap<String, Character> strMap = new HashMap<>();
+		 
+		 String[] strArray = str.split(" ");
+		 if(strArray.length != pattern.length()){
+			 return false;
+		 }
+		 
+		 int i = 0;
+		 while(i < strArray.length){
+			 patternMap.put(pattern.charAt(i), strArray[i]);
+			 strMap.put(strArray[i], pattern.charAt(i));
+			 i++;
+		 }
+		 
+		 for(int j = 0; j < strArray.length; j++){
+			 if(!patternMap.get(pattern.charAt(j)).equals(strArray[j])){
+				 return false;
+			 }
+			 if(strMap.get(strArray[j]) != pattern.charAt(j)){
+				 return false;
+			 }
+		 }
+		 
+		 return true;
+	 }
+}
