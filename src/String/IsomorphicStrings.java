9772a68546c8e32aeab692669985cd51431d5fae
AlexXiao2016 <xpusjobs@gmail.com>
2016-09-25 17:20:54 +0800

    Isomorphic Strings

---
+++ src/String/IsomorphicStrings.java
@@ -0,0 +1,27 @@
+package String;
+
+import java.util.HashMap;
+
+public class IsomorphicStrings {
+	public boolean isIsomorphic(String s, String t) {
+		HashMap<Character, Character> mapS = new HashMap<>();
+		HashMap<Character, Character> mapT = new HashMap<>();
+		
+		for(int i = 0; i < s.length(); i++){
+			mapS.put(s.charAt(i), t.charAt(i));
+			mapT.put(t.charAt(i), s.charAt(i));
+		}
+		
+		for(int i = 0; i < s.length(); i++){
+			if(mapS.get(s.charAt(i)) != t.charAt(i)){
+				return false;
+			}
+			if(mapT.get(t.charAt(i)) != s.charAt(i)){
+				return false;
+			}
+		}
+		
+		
+		return true;
+	}
+}
