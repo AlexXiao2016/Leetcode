fe968576c291f6b6b2a9ec8e7d26dc9a7347453c
AlexXiao2016 <xpusjobs@gmail.com>
2016-09-29 11:26:51 -0700

    Add one more method for Kth Largest Element in an Array

--- src/Array/KthLargestElementinanArray.java
+++ src/Array/KthLargestElementinanArray.java
@@ -5,15 +5,35 @@ import java.util.PriorityQueue;
 public class KthLargestElementinanArray {
 	public int findKthLargest(int[] nums, int k) {
 		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		
+
 		for(int i = 0; i < nums.length; i++){
 			pq.add(nums[i]);
 		}
-		
+
 		while(pq.size() > k){
 			pq.poll();
 		}
-		
+
+		return pq.poll();
+	}
+
+	public int findKthLargest2(int[] nums, int k) {
+
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+		for(int i = 0; i < nums.length; i++){
+			if(pq.size() < k){
+				pq.offer(nums[i]);
+			}else{
+				int temp = pq.peek();
+				if(temp < nums[i]){
+					pq.poll();
+					pq.offer(nums[i]);
+				}
+			}		
+		}
+
 		return pq.poll();
 	}
 }
