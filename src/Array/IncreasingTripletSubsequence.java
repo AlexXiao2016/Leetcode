3f2e732166909de5434fb9106c2999aab01ec1ff
AlexXiao2016 <xpusjobs@gmail.com>
2016-09-29 11:01:05 -0700

    Increasing Triplet Subsequence

---
+++ src/Array/IncreasingTripletSubsequence.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class IncreasingTripletSubsequence {
+	public boolean increasingTriplet(int[] nums) {
+		if(nums == null || nums.length < 3){
+			return false;
+		}
+		
+		int min = Integer.MAX_VALUE, secondMin = Integer.MAX_VALUE;
+		for(int i = 0; i < nums.length; i++){
+			if(nums[i] <= min){
+				min = nums[i];
+			}else if(nums[i] <= secondMin){
+				secondMin = nums[i];
+			}else if(nums[i] > secondMin){
+				return true;
+			}
+		}
+		
+		return false;
+	}
+}
+ 
