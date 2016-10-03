ba6c6e75a6fa8b530472290853bd785f8b1dc857
AlexXiao2016 <xpusjobs@gmail.com>
2016-10-02 22:23:37 -0700

    Add a new recursive way for Combination Sum IV

--- src/Array/CombinationSumIV.java
+++ src/Array/CombinationSumIV.java
@@ -7,12 +7,12 @@ public class CombinationSumIV {
 	public int combinationSum4(int[] nums, int target) {
 		Arrays.sort(nums);
 		List<List<Integer>> result = new ArrayList<>();
-		
+
 		dfs(nums, target, 0, new ArrayList<Integer>(), result);
 		return result.size();
 	}
-	
-	
+
+
 	public void dfs(int[] nums, int target, int start, List<Integer> list, List<List<Integer>> result){
 		if(target == 0){
 			List<Integer> tep = new ArrayList<>(list);
@@ -25,5 +25,18 @@ public class CombinationSumIV {
 				temp.remove(temp.size() - 1);
 			}
 		}
+	}
+
+	public int combinationSum4_2(int[] nums, int target) {
+		if (target == 0) {
+	        return 1;
+	    }
+	    int res = 0;
+	    for (int i = 0; i < nums.length; i++) {
+	        if (target >= nums[i]) {
+	            res += combinationSum4(nums, target - nums[i]);
+	        }
+	    }
+	    return res;
 	}
 }
