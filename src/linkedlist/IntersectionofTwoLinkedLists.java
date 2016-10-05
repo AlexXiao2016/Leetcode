36c13e6f1ae181fb706f1031df1707b4e2e8c7a4
AlexXiao2016 <xpusjobs@gmail.com>
2016-10-04 23:31:56 -0700

    no changes

--- src/linkedlist/IntersectionofTwoLinkedLists.java
+++ src/linkedlist/IntersectionofTwoLinkedLists.java
@@ -1,5 +1,40 @@
 package linkedlist;
 
-public class IntersectionofTwoLinkedLists {
+import java.util.HashMap;
 
+public class IntersectionofTwoLinkedLists {
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		if(headA == null || headB == null){
+			return null;
+		}
+		
+		HashMap<ListNode, Boolean> map = new HashMap<>();
+		
+		while(headA != null){
+			if(!map.containsKey(headA)){
+				map.put(headA, true);
+			}
+			headA = headA.next;
+		}
+		
+		while(headB != null){
+			if(map.containsKey(headB)){
+				return headB;
+			}
+			headB = headB.next;
+		}
+		
+		
+		return null;
+	}
+}
+
+
+class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
 }
