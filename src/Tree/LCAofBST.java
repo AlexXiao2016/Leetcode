93d8d25fee8dc7ea0d3422bef0ca2462b5d65c53
AlexXiao2016 <xpusjobs@gmail.com>
2016-10-06 15:39:27 -0700

    Integer to Roman

--- src/Tree/LCAofBST.java
+++ src/Tree/LCAofBST.java
@@ -2,4 +2,26 @@ package Tree;
 
 public class LCAofBST {
 
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if(root == null || p == null || q == null){
+			return null;
+		}
+		if(p.val < root.val && q.val < root.val){
+			return lowestCommonAncestor(root.left, p, q);
+		}else if(p.val > root.val && q.val > root.val){
+			return lowestCommonAncestor(root.left, p, q);
+		}else{
+			return root;
+		}
+	}
 }
+
+
+// Definition for a binary tree node.
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int x) { val = x; }
+}
+
