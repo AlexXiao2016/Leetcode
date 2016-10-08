cb4e22880d1c37e092eb8cc6784d90e794116393
AlexXiao2016 <xpusjobs@gmail.com>
2016-10-07 23:45:00 -0700

    add reverse list from middle

--- src/linkedlist/ReverseLinkedList.java
+++ src/linkedlist/ReverseLinkedList.java
@@ -1,5 +1,39 @@
 package linkedlist;
 
 public class ReverseLinkedList {
+	public ListNode reverseList(ListNode head) {
 
+		ListNode newHead = null;
+
+		while(head != null){
+			ListNode tmp = head.next;
+			head.next = newHead;
+			newHead = head;
+			head = tmp;
+		}
+		
+		return newHead;
+	}
+	
+	
+	public ListNode reverseListFromMid(ListNode head){
+		
+		ListNode slow = head;
+		ListNode fast = head;
+		
+		while(fast != null){
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		
+		fast = slow;
+		ListNode midHead = null;
+		while(slow.next != null){
+			ListNode temp = slow.next.next;
+			
+		}
+		
+		
+		return head;
+	}
 }
