e50bb22049b288f683df74805e3056dafa196442
AlexXiao2016 <xpusjobs@gmail.com>
2016-10-10 01:16:50 -0700

    Path Sum II

---
+++ src/Tree/PathSumII.java
@@ -0,0 +1,26 @@
+package Tree;
+
+import java.util.*;
+
+public class PathSumII {
+	public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> result = new ArrayList<>();
+        helper(root, sum, new ArrayList<Integer>(), result);
+        return result;
+    }
+    
+    public void helper(TreeNode root, int sum, List<Integer> list, List<List<Integer>> result){
+        if(root == null) return;
+        
+        list.add(root.val);
+        
+        if(root.left == null && root.right == null && sum == root.val){
+            result.add(new ArrayList<Integer>(list));
+        }else{
+            helper(root.left, sum - root.val, list, result);
+            helper(root.right, sum - root.val, list, result);
+        }
+        
+        list.remove(list.size() - 1);
+    }
+}
