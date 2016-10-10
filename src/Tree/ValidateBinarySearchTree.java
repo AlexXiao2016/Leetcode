37bc6f6b34e42440912c993c8b1d697fea2c1264
AlexXiao2016 <xpusjobs@gmail.com>
2016-10-09 23:57:05 -0700

    Validate Binary Search Tree recursive

---
+++ src/Tree/ValidateBinarySearchTree.java
@@ -0,0 +1,15 @@
+package Tree;
+
+public class ValidateBinarySearchTree {
+	public boolean isValidBST(TreeNode root) {
+        return helper(root, null, null);
+    }
+    
+    public boolean helper(TreeNode node, Integer low, Integer high){
+        if(node == null){
+            return true;
+        }
+        return (low == null || node.val > low) && (high == null || node.val < high) && helper(node.left, low, node.val) && helper(node.right, node.val, high);
+        
+    }
+}
