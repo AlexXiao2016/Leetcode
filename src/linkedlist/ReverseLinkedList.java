74430dc773604d47e78a209373b95f109450cf37
AlexXiao2016 <xpusjobs@gmail.com>
2016-10-12 18:52:13 -0700

    reverse List From Mid

--- src/linkedlist/ReverseLinkedList.java
+++ src/linkedlist/ReverseLinkedList.java
@@ -16,24 +16,51 @@ public class ReverseLinkedList {
 	}
 	
 	
-	public ListNode reverseListFromMid(ListNode head){
+	public static ListNode reverseListFromMid(ListNode head){
 		
 		ListNode slow = head;
 		ListNode fast = head;
 		
-		while(fast != null){
+		while(fast.next != null && fast.next.next != null){
 			slow = slow.next;
 			fast = fast.next.next;
 		}
 		
-		fast = slow;
-		ListNode midHead = null;
-		while(slow.next != null){
-			ListNode temp = slow.next.next;
-			
+		ListNode rightHalfHead = slow.next;
+		slow.next = null;
+		ListNode newHead = null;
+		while(rightHalfHead != null){
+			ListNode temp = rightHalfHead.next;
+			rightHalfHead.next = newHead;
+			newHead = rightHalfHead;
+			rightHalfHead = temp;			
+		}
+		slow.next = newHead;		
+		return head;
+	}
+	
+	
+	public static void main(String args[]){
+		ListNode n1 = new ListNode(1);
+		ListNode n2 = new ListNode(2);
+		ListNode n3 = new ListNode(3);
+		ListNode n4 = new ListNode(4);
+		ListNode n5 = new ListNode(5);
+		
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
+		n5.next = null;
+		
+		reverseListFromMid(n1);
+		
+		while(n1 != null){
+			System.out.print(n1.val + " ");	
+			n1 = n1.next;
 		}
 		
 		
-		return head;
+		
 	}
 }
