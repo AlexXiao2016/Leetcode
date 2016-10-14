99970716578fdee57230177e21db4a8e850ec595
AlexXiao2016 <xpusjobs@gmail.com>
2016-10-14 16:24:04 -0700

    Partition Equal Subset Sum

---
+++ src/linkedlist/PartitionEqualSubsetSum.java
@@ -0,0 +1,32 @@
+package linkedlist;
+
+public class PartitionEqualSubsetSum {
+	
+	public static boolean canPartition(int[] nums) {
+		if(nums == null || nums.length == 0){
+			return true;
+		}		
+		
+		int sum  = 0;
+		for(int i : nums){
+			sum += i;
+		}
+		if(sum % 2 != 0){
+			return false;
+		}
+		sum /= 2;
+		boolean dp[] = new boolean[sum + 1];
+		dp[0] = true;
+		for(int i = 0; i < nums.length; i++){
+			for(int j = sum; j >= nums[i]; j--){
+				dp[j] = dp[j] || dp[j - nums[i]];
+			}
+		}		
+		return dp[sum];		
+	}
+	
+	public static void main(String args[]){
+		int nums[] = {1, 2, 3, 4};
+		System.out.println(canPartition(nums));
+	}
+}
