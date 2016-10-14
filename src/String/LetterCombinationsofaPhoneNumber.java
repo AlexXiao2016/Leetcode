59c57eab755671c47841a3fbf9dbb723bae4bcb8
AlexXiao2016 <xpusjobs@gmail.com>
2016-10-13 23:16:20 -0700

    Letter Combinations of a Phone Number

---
+++ src/String/LetterCombinationsofaPhoneNumber.java
@@ -0,0 +1,28 @@
+package String;
+
+import java.util.*;
+
+public class LetterCombinationsofaPhoneNumber {
+	public static List<String> letterCombinations(String digits) {
+		LinkedList<String> result = new LinkedList<>();
+		if(digits == null || digits.length() == 0){
+			return result;
+		}
+		String[] map = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+		result.add("");
+		for(int i = 0; i < digits.length(); i++){
+			int index = digits.charAt(i) - '0';
+			while(result.peek().length() == i){
+				String temp = result.remove();
+				for(char c : map[index].toCharArray()){
+					result.add(temp + c);
+				}
+			}			
+		}		
+		return result;
+	}	
+	
+	public static void main(String args[]){
+		letterCombinations("1023123");
+	}
+}	
