52c775d7f69ece2905409a8415c16cbd4284a051
AlexXiao2016 <xpusjobs@gmail.com>
2016-10-17 22:57:15 -0700

    Verify Preorder Serialization of a Binary Tree

---
+++ src/Tree/VerifyPreorderSerializationofaBinaryTree.java
@@ -0,0 +1,31 @@
+package Tree;
+
+import java.util.Stack;
+
+public class VerifyPreorderSerializationofaBinaryTree {
+	public static boolean isValidSerialization(String preorder) {
+		
+		if(preorder == null || preorder.length() == 0){
+			return false;
+		}
+		
+		Stack<String> s = new Stack<>();
+		String str[] = preorder.split(",");
+		for(int i = 0; i < str.length; i++){
+			String curr = str[i];
+			while(curr.equals("#") && !s.isEmpty() && s.peek().equals(curr)){
+				s.pop();
+				if(s.isEmpty()){
+					return false;
+				}
+				s.pop();
+			}
+			s.push(curr);
+		}
+		return s.size() == 1 && s.peek().equals("#");
+	}
+	
+	public static void main(String args[]){
+		System.out.println(isValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#"));
+	}
+}
