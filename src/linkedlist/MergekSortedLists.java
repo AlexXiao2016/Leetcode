3d8d1f50d768a72b11ddfad036be7cbb02b6566e
AlexXiao2016 <xpusjobs@gmail.com>
2016-10-18 22:30:48 -0700

    Merge k Sorted Lists

---
+++ src/linkedlist/MergekSortedLists.java
@@ -0,0 +1,32 @@
+package linkedlist;
+
+import java.util.PriorityQueue;
+
+public class MergekSortedLists {
+	public ListNode mergeKLists(ListNode[] lists) {
+		if(lists == null || lists.length == 0){
+			return null;
+		}
+		PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+		
+		for(ListNode node : lists){
+			if(node != null){
+				pq.add(node);
+			}
+		}
+		
+		ListNode fakeHead = new ListNode(0);
+		ListNode curr = fakeHead;
+		while(!pq.isEmpty()){
+			ListNode temp = pq.poll();
+			curr.next = temp;
+			curr = curr.next;
+			
+			if(temp.next != null){
+				pq.offer(temp);
+			}
+		}
+		
+		return fakeHead.next;
+	}
+}
