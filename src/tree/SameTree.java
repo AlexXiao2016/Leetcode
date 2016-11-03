f0d947cf18f75ea3ff28887dd7b4880c5dbdbe25
AlexXiao2016 <xpusjobs@gmail.com>
2016-11-02 17:26:57 -0700

    Same Tree

---
+++ src/tree/SameTree.java
@@ -0,0 +1,13 @@
+package tree;
+
+public class SameTree {
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if(p == null && q == null){
+			return true;
+		}else if(p == null || q == null || p.val != q.val){
+			return false;
+		}else{
+			return true && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+		}
+	}
+}
