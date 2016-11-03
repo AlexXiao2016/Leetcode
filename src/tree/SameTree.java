c4cd363f04fc22d080280eb61bb74de2a91d24f8
AlexXiao2016 <xpusjobs@gmail.com>
2016-11-02 17:27:54 -0700

    Same Tree

--- src/tree/SameTree.java
+++ src/tree/SameTree.java
@@ -7,7 +7,7 @@ public class SameTree {
 		}else if(p == null || q == null || p.val != q.val){
 			return false;
 		}else{
-			return true && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+			return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 		}
 	}
 }
