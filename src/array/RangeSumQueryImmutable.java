ba84168bf42d483c754161871621feb1a2d199c6
AlexXiao2016 <xpusjobs@gmail.com>
2016-11-04 00:25:42 -0700

    Range Sum Query - Immutable

---
+++ src/array/RangeSumQueryImmutable.java
@@ -0,0 +1,25 @@
+package array;
+
+public class RangeSumQueryImmutable {
+	int sum[];
+    int n;
+    
+    public RangeSumQueryImmutable(int[] nums) {
+        n = nums.length;
+        if(n == 0){
+            return;
+        }
+        sum = new int[n];
+        sum[0] = nums[0];
+        for(int i = 1; i < n; i++){
+            sum[i] = sum[i - 1] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        if(i == 0){
+            return sum[j];
+        }
+        return sum[j] - sum[i - 1];
+    }
+}
