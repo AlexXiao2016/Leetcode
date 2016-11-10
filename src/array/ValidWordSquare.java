27ed9d615519c32a6078abae1b9af59f4131fb1e
AlexXiao2016 <xpusjobs@gmail.com>
2016-11-09 20:51:03 -0800

    valid Word Square

---
+++ src/array/ValidWordSquare.java
@@ -0,0 +1,21 @@
+package array;
+
+import java.util.*;
+
+public class ValidWordSquare {
+	public boolean validWordSquare(List<String> words) {
+		if(words == null || words.size() == 0){
+            return true;
+        }
+        
+        for(int i = 0; i < words.size(); i++){
+            for(int j = 0; j < words.get(i).length(); j++){
+                if(j >= words.size() || i >= words.get(j).length() ||words.get(i).charAt(j) != words.get(j).charAt(i)){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
