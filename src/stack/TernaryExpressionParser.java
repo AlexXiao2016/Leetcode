417777b61a5b42e8d4d753bb8f633bc20a66a8e5
AlexXiao2016 <xpusjobs@gmail.com>
2016-11-11 10:14:05 -0800

    Paint House

--- src/stack/TernaryExpressionParser.java
+++ src/stack/TernaryExpressionParser.java
@@ -27,8 +27,19 @@ public class TernaryExpressionParser {
 		return String.valueOf(stack.peek());
 	}
 	
+	public static String parseTernary2(String expression) {
+        while (expression.length() != 1) {
+            int i = expression.lastIndexOf("?");    // get the last shown '?'
+            char tmp;
+            if (expression.charAt(i-1) == 'T') { tmp = expression.charAt(i+1); }
+            else { tmp = expression.charAt(i+3); }
+            expression = expression.substring(0, i-1) + tmp + expression.substring(i+4);
+        }
+        return expression;
+    }
+	
 	public static void main(String args[]){
-		System.out.println(parseTernary("F?T:F?T?1:2:F?3:4"));
+		System.out.println(parseTernary2("F?T:F?T?1:2:F?3:4"));
 	}
 	
 	
