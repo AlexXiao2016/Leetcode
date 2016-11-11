f1cdf7447ce53a5e00beda51aa57cc35d29b7a80
AlexXiao2016 <xpusjobs@gmail.com>
2016-11-11 12:01:17 -0800

    Flip Game II

---
+++ src/backtracking/FlipGameII.java
@@ -0,0 +1,39 @@
+package backtracking;
+
+import java.util.*;
+
+public class FlipGameII {
+	public boolean canWin(String s) {
+		if(s == null || s.length() < 2){
+			return false;
+		}
+		
+		HashMap<String, Boolean>  map = new HashMap<>();
+		return canWin(s, map);
+	}
+	
+	public boolean canWin(String s, HashMap<String, Boolean> map){
+		if(map.containsKey(s)){
+			return map.get(s);
+		}
+		
+		for(int i = 0; i < s.length() - 1; i++){
+			if(s.startsWith("++", i)){
+				String tmp = s.substring(0, i) + "--" + s.substring(i + 2);
+				if(!canWin(tmp, map)){
+					map.put(tmp, true);
+					return true;
+				}
+			}
+		}
+		map.put(s, false);
+		return false;
+	}
+	
+	public static void main(String args[]){
+		String s = "++++";
+		FlipGameII fg = new FlipGameII();
+		System.out.println(fg.canWin(s));
+	}
+	
+}
