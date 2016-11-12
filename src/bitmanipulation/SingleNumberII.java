832a92e24faa2c9db2e414a1aa908d4414efa757
AlexXiao2016 <xpusjobs@gmail.com>
2016-11-11 23:01:33 -0800

    Single Number II

---
+++ src/bitmanipulation/SingleNumberII.java
@@ -0,0 +1,13 @@
+package bitmanipulation;
+
+public class SingleNumberII {
+	public int singleNumber(int[] nums) {
+		int one = 0, two = 0;
+		for(int i = 0; i < nums.length; i++){
+			one = (one ^ nums[i]) & ~two;
+			two = (two ^ nums[i]) & ~one;			
+		}
+		
+		return one;
+	}
+}
