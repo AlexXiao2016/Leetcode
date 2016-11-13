addd31e92ade6fae804d36d4dc25df4044851ef1
AlexXiao2016 <xpusjobs@gmail.com>
2016-11-12 22:09:05 -0800

    Longest Increasing Subsequence

---
+++ src/dp/LongestIncreasingSubsequence.java
@@ -0,0 +1,41 @@
+package dp;
+import java.util.*;
+
+public class LongestIncreasingSubsequence {
+	public static int lengthOfLIS(int[] nums) {            
+        int[] dp = new int[nums.length];
+        int len = 0;
+
+        for(int x : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, x);
+            if(i < 0) i = -(i + 1);
+            dp[i] = x;
+            if(i == len) len++;
+        }
+
+        return len;
+    }
+	
+	public static int lengthOfLIS2(int[] nums) {
+	    int[] tails = new int[nums.length];
+	    int size = 0;
+	    for (int x : nums) {
+	        int i = 0, j = size;
+	        while (i != j) {
+	            int m = (i + j) / 2;
+	            if (tails[m] < x)
+	                i = m + 1;
+	            else
+	                j = m;
+	        }
+	        tails[i] = x;
+	        if (i == size) ++size;
+	    }
+	    return size;
+	}
+	
+	public static void main(String args[]){
+		int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+		lengthOfLIS2(nums);
+	}
+}
