d45bdb59b9be994e9289ba7a4d68541e1ccf70db
AlexXiao2016 <xpusjobs@gmail.com>
2016-11-13 20:30:32 -0800

    Single Number II

--- src/bitmanipulation/SingleNumberII.java
+++ src/bitmanipulation/SingleNumberII.java
@@ -7,7 +7,29 @@ public class SingleNumberII {
 			one = (one ^ nums[i]) & ~two;
 			two = (two ^ nums[i]) & ~one;			
 		}
-		
+
 		return one;
+	}
+
+	public int singleNumber2(int[] nums){
+		int result = 0;
+		for (int i = 0; i < 32; i++) {
+			int count = 0;
+			for (int j = 0; j < nums.length; j++) {
+				count += ((nums[j] >> i) & 1);
+			}
+			result |= ((count % 3) << i);
+		}
+		return result;
+	}
+	
+	public static void main(String args[]){
+		SingleNumberII sn = new SingleNumberII();
+//		
+//		int[] nums = {1,2,3,2,2,1,1} ;
+//		sn.singleNumber2(nums);
+		
+		int num = 10;
+		System.out.println(~num);
 	}
 }
