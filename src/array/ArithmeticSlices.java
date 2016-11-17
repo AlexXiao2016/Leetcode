b9ed6f122dd9e2d486c668193b48075539f61950
AlexXiao2016 <xpusjobs@gmail.com>
2016-11-16 23:56:34 -0800

    update atoi

--- src/array/ArithmeticSlices.java
+++ src/array/ArithmeticSlices.java
@@ -14,4 +14,29 @@ public class ArithmeticSlices {
         
         return sum;
 	}
+	
+	
+	public static int numberOfArithmeticSlices2(int[] A) {
+        if(A == null || A.length < 3){
+            return 0;
+        }
+        
+        int count = 1;
+        int sum = 0;
+        for(int i = 2; i < A.length; i++){
+            if(A[i] - A[i - 1] == A[i - 1] - A[i - 2]){
+                sum += count;
+                count++;
+            }else{
+                count = 1;
+            }
+        }
+        
+        return sum;
+    }
+	
+	public static void main(String args[]){
+		int[] nums = {1,2,3,4};
+		numberOfArithmeticSlices2(nums);
+	}
 }
--- src/string/StringtoInteger.java
+++ src/string/StringtoInteger.java
@@ -2,25 +2,25 @@ package string;
 
 public class StringtoInteger {
 	public int myAtoi(String str) {
-        if(str.isEmpty()){
-            return 0;
-        }
-        int i = 0, sign = 1, base = 0;
-        while(str.charAt(i) == ' '){
-            i++;
-        }
-        if(str.charAt(i) == '+' || str.charAt(i) == '-'){
-            sign = str.charAt(i++) == '+' ? 1 : -1;
-        }
-        
-        int max = Integer.MAX_VALUE;
-        int min = Integer.MIN_VALUE;
-        while(i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9'){
-            if(base > max / 10 || (base == max / 10 && str.charAt(i) > '7')){
-                return sign == 1? max : min;
-            }
-            base = base * 10 + str.charAt(i++) - '0';
-        }
-        return base * sign;
-    }
+		if(str.isEmpty()){
+			return 0;
+		}
+		int i = 0, sign = 1, base = 0;
+		while(str.charAt(i) == ' '){
+			i++;
+		}
+		if(str.charAt(i) == '+' || str.charAt(i) == '-'){
+			sign = str.charAt(i++) == '+' ? 1 : -1;
+		}
+
+		int max = Integer.MAX_VALUE;
+		int min = Integer.MIN_VALUE;
+		while(i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9'){
+			if(base > max / 10 || (base == max / 10 && str.charAt(i) > '7')){
+				return sign == 1? max : min;
+			}
+			base = base * 10 + str.charAt(i++) - '0';
+		}
+		return base * sign;
+	}
 }
