3c2caef359651856ae12878453ccb3e14aa334a6
AlexXiao2016 <xpusjobs@gmail.com>
2016-11-17 18:56:42 -0800

    Encodeand Decode Strings

---
+++ src/string/EncodeandDecodeStrings.java
@@ -0,0 +1,28 @@
+package string;
+import java.util.*;
+
+public class EncodeandDecodeStrings {
+	// Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for(String str : strs){
+        	sb.append(str.length());
+        	sb.append('/');
+        	sb.append(str);
+        }
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> res = new ArrayList<>();
+        int i = 0;
+        while(i < s.length()){
+        	int index = s.indexOf('/', i);
+        	int size = Integer.valueOf(s.substring(i, index));
+        	res.add(s.substring(index + 1, index + size + 1));
+        	i = index + size + 1;
+        }
+        return res;
+    }
+}
