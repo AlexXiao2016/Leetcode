43cf468892e38257f364ca8d2066d53ea2d4f34f
AlexXiao2016 <xpusjobs@gmail.com>
2016-11-21 09:33:24 -0800

    Linked List Cycle I && II

---
+++ src/linkedlist/ListedListCycleII.java
@@ -0,0 +1,24 @@
+package linkedlist;
+
+public class ListedListCycleII {
+	public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null) return null;
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode start = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast){
+                while(start != slow){
+                    start = start.next;
+                    slow = slow.next;
+                }
+                return start;
+            }
+        }
+        return null;
+    }
+}
