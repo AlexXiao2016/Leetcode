d54ae23c251c4620944f7834d685807f01cdf8c7
AlexXiao2016 <xpusjobs@gmail.com>
2016-11-20 20:27:10 -0800

    Sliding Window Maximum

---
+++ src/queue/SlidingWindowMaximum.java
@@ -0,0 +1,37 @@
+package queue;
+import java.util.*;
+
+public class SlidingWindowMaximum {
+
+	public static int[] maxSlidingWindow(int[] nums, int k) {
+		if(nums == null || nums.length == 0 || k <=0){
+			return null;
+		}
+
+		int n = nums.length;
+		int res[] = new int[n - k + 1];
+		Deque<Integer> dq = new LinkedList<>();
+		int j = 0;
+		for(int i = 0; i < n; i++){
+			while(!dq.isEmpty() && dq.peek() < i - k + 1){
+				dq.poll();
+			}
+
+			while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]){
+				dq.pollLast();
+			}
+
+			dq.offer(i);
+			if(i >= k - 1){
+				res[j++] = nums[dq.peek()];
+			}
+		}
+
+		return res;
+	}
+
+	public static void main(String args[]){
+		int[] nums = {1,3,-1,-3,5,3,6,7};
+		maxSlidingWindow2(nums, 3);
+	}
+}
