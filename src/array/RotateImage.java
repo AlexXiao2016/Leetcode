02a03a27aaac295e5ff9cf199e6459633c3c238d
AlexXiao2016 <xpusjobs@gmail.com>
2016-11-21 23:11:31 -0800

    Rotate Matrix and Sliding Window Maximum

--- src/array/RotateImage.java
+++ src/array/RotateImage.java
@@ -21,4 +21,8 @@ public class RotateImage {
 			}
 		}
 	}
+	
+	public void rotate(int[][] matrix, int flag){
+		
+	}
 }
--- src/queue/SlidingWindowMaximum.java
+++ src/queue/SlidingWindowMaximum.java
@@ -32,6 +32,6 @@ public class SlidingWindowMaximum {
 
 	public static void main(String args[]){
 		int[] nums = {1,3,-1,-3,5,3,6,7};
-		maxSlidingWindow2(nums, 3);
+		maxSlidingWindow(nums, 3);
 	}
 }
