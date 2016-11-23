bed8ad672000d83cc04730c6452273ff9c9ab71b
AlexXiao2016 <xpusjobs@gmail.com>
2016-11-23 14:28:49 -0800

    Course Schedule

--- src/graph/CourseSchedule.java
+++ src/graph/CourseSchedule.java
@@ -2,7 +2,7 @@ package graph;
 import java.util.*;
 
 public class CourseSchedule {
-	public boolean canFinish(int numCourses, int[][] prerequisites) {
+	public boolean canFinishBFS(int numCourses, int[][] prerequisites) {
 		int[][] matrix = new int[numCourses][numCourses]; // i -> j
 	    int[] indegree = new int[numCourses];
 	    
@@ -15,7 +15,7 @@ public class CourseSchedule {
 	    }
 	    
 	    int count = 0;
-	    Queue<Integer> queue = new LinkedList();
+	    Queue<Integer> queue = new LinkedList<>();
 	    for (int i=0; i<indegree.length; i++) {
 	        if (indegree[i] == 0) queue.offer(i);
 	    }
@@ -30,5 +30,47 @@ public class CourseSchedule {
 	        }
 	    }
 	    return count == numCourses;
+	}
+	
+	
+	public boolean canFinishDFS(int numCourses, int[][] prerequisities){
+		if(numCourses == 0 || prerequisities == null || prerequisities.length == 0 || prerequisities[0].length == 0) {
+			return true;
+		}
+		
+		List<List<Integer>> courses = new ArrayList<>();
+		for(int i = 0; i < numCourses; i++){
+			courses.add(new ArrayList<Integer>());
+		}
+		
+		for(int i = 0; i < prerequisities.length; i++){
+			courses.get(prerequisities[i][1]).add(prerequisities[i][0]);
+		}
+		
+		int visited[] = new int[numCourses];
+		
+		for(int i = 0; i < numCourses; i++){
+			if(!dfs(i, courses, visited)) return false;
+		}
+		
+		return true;
+	}
+	
+	public boolean dfs(int course, List<List<Integer>> courses, int visited[]){
+		visited[course] = 1;
+		
+		List<Integer> eligibleCourses = courses.get(course);
+		
+		for(int i = 0; i < eligibleCourses.size(); i++){
+			int eligibleCourse = eligibleCourses.get(i);
+			
+			if(visited[eligibleCourse] == 1) return false;
+			if(visited[eligibleCourse] == 0){ // not visited
+				if(!dfs(eligibleCourse, courses, visited)) return false;
+			}
+		}
+		
+		visited[course] = 2;
+		return true;
 	}
 }
