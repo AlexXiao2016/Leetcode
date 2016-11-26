433a0c619c49aef649e882d7a9477507fc3daaa3
AlexXiao2016 <xpusjobs@gmail.com>
2016-11-26 00:48:19 -0800

    Multiply Strings

---
+++ src/string/MultiplyStrings.java
@@ -0,0 +1,29 @@
+package string;
+
+public class MultiplyStrings {
+	public String multiply(String num1, String num2) {
+		int m = num1.length();
+		int n = num2.length();
+		
+		int[] pos = new int[m + n];
+		for(int i = m - 1; i >= 0; i--){
+			for(int j =  n -1; j >= 0; j--){
+				int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+				int p1 = i + j, p2 = i + j + 1;
+				int sum = mul + pos[p2];
+				
+				pos[p1] += sum / 10;
+				pos[p2] = sum % 10;
+			}
+		}
+		
+		StringBuilder res = new StringBuilder();
+		for(int i = 0; i < pos.length; i++){
+			if(!(res.length() == 0 && pos[i] == 0)){
+				res.append(pos[i]);
+			}
+		}
+		
+		return res.length() == 0? "0" : res.toString();
+	}
+}
