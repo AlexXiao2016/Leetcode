8ef71cc9455ad42bc5aaac67b9b92103c1cb3bf8
AlexXiao2016 <xpusjobs@gmail.com>
2016-11-30 23:13:59 -0800

    Word Break

---
+++ src/dp/WordBreak.java
@@ -0,0 +1,28 @@
+package dp;
+import java.util.*;
+
+public class WordBreak {
+	public boolean wordBreak(String s, Set<String> wordDict) {
+		boolean dp[] = new boolean[s.length() + 1];
+		dp[0] = true;
+		
+		for(int i = 1; i <= s.length(); i++){
+			for(int j = 0; j < i; j++){
+				if(dp[j] && wordDict.contains(s.substring(j, i))){
+					dp[i] = true;
+					break;
+				}
+			}
+		}
+		
+		return dp[s.length()];
+	}
+	
+	public static void main(String args[]){
+		WordBreak wb = new WordBreak();
+		Set<String> dict = new HashSet<>();
+		dict.add("Leet");
+		dict.add("code");
+		wb.wordBreak("Leetcode", dict);
+	}
+}
