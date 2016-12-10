993908e8a5e9613f67fb957918f1476ae3e4c3a4
Ping Xiao <xpusjobs@gmail.com>
2016-12-10 02:47:45 -0800

    Valid BST InOrder traversal and Word Break new DP

--- src/dp/WordBreak.java
+++ src/dp/WordBreak.java
@@ -18,6 +18,25 @@ public class WordBreak {
 		return dp[s.length()];
 	}
 	
+	public boolean wordBreak2(String s, Set<String> wordDict) {
+		boolean res[] = new boolean[s.length() + 1];
+		res[0] = true;
+		
+		for(int i = 1; i <= s.length(); i++){
+			for(String word: wordDict){
+				if(word.length() <= i){
+					if(res[i - word.length()]){
+						if(s.substring(i - word.length(), i).equals(word)){
+							res[i] = true;
+							break;
+						}
+					}
+				}
+			}
+		}
+		return res[s.length()];
+	}
+	
 	public static void main(String args[]){
 		WordBreak wb = new WordBreak();
 		Set<String> dict = new HashSet<>();
--- src/tree/ValidateBinarySearchTree.java
+++ src/tree/ValidateBinarySearchTree.java
@@ -1,4 +1,5 @@
 package tree;
+import java.util.*;
 
 public class ValidateBinarySearchTree {
 	public boolean isValidBST(TreeNode root) {
@@ -11,5 +12,44 @@ public class ValidateBinarySearchTree {
         }
         return (low == null || node.val > low) && (high == null || node.val < high) && helper(node.left, low, node.val) && helper(node.right, node.val, high);
         
+    }
+    
+    public boolean isValidBSTInOrder(TreeNode root){
+    	if(root == null) return true;
+    	
+    	Stack<TreeNode> stack = new Stack<>();
+    	TreeNode cur = root;
+    	
+    	while(cur != null){
+    		stack.push(cur);
+    		cur = cur.left;
+    	}
+    	
+    	TreeNode pre = null;
+    	while(!stack.isEmpty()){
+    		TreeNode temp = stack.pop();
+    		if(pre != null && pre.val >= temp.val) return false;
+    		pre = temp;
+    		if(temp.right != null){
+    			TreeNode right = temp.right;
+    			
+    			while(right != null){
+    				stack.push(right);
+    				right = right.left;
+    			}
+    		}
+    	}
+    	return true;
+    }
+    
+    public static void main(String args[]){
+    	TreeNode root = new TreeNode(2);
+    	TreeNode t1 = new TreeNode(2);
+//    	TreeNode t2 = new TreeNode(3);
+    	
+    	root.left = t1;
+//    	root.right = t2;
+    	ValidateBinarySearchTree vBst = new ValidateBinarySearchTree();
+    	vBst.isValidBSTInOrder(root);
     }
 }
