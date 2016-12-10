53a614385a51192d31a5d5146bc1ce8a7cc1ea89
pingxiao <pingxiao@10.0.0.8>
2016-12-09 20:33:08 -0800

    Find All Anagrams in a String update

--- src/string/FindAllAnagramsinaString.java
+++ src/string/FindAllAnagramsinaString.java
@@ -1,23 +1,46 @@
 package string;
+
 import java.util.*;
 
 public class FindAllAnagramsinaString {
 	public List<Integer> findAnagrams(String s, String p) {
 		List<Integer> result = new ArrayList<Integer>();
-		if(s == null || p == null || s.length() < p.length()) return result;
-		
+		if (s == null || s.length() == 0  || p == null || p.length() == 0 || s.length() < p.length())
+			return result;
+
 		char pc[] = p.toCharArray();
-		int map[] = new int[256];
-		for(int i = 0; i < pc.length; i++){
-			map[pc[i]]++; 
+		int map[] = new int[26];
+		for (int i = 0; i < pc.length; i++) {
+			map[pc[i] - 'a']++;
 		}
+
+		int left = 0, right = 0;
+		int count = p.length();
 		
-		int start = 0, end = 0, count = p.length();
-		while(end < s.length()){
-			if(map[s.charAt(end++)]-- >= 1) count--;
-			if(count == 0) result.add(start);
-			if(end - start == p.length() && map[s.charAt(start++)]++ >= 0) count++;
+		while (right < s.length()) {
+			if (map[s.charAt(right) - 'a'] > 0) {
+				count--;
+			}
+			map[s.charAt(right) - 'a']--;
+			right++;
+
+			if (count == 0) {
+				result.add(left);
+			}
+
+			if (right - left == p.length()) {
+				if (map[s.charAt(left) - 'a'] >= 0) {
+					count++;
+				}
+				map[s.charAt(left) - 'a']++;
+				left++;
+			}
 		}
 		return result;
+	}
+
+	public static void main(String args[]) {
+		FindAllAnagramsinaString faas = new FindAllAnagramsinaString();
+		faas.findAnagrams("cbaebabacd", "abc");
 	}
 }
