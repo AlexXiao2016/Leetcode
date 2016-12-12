308b98820e78b39dc7db8816f2da81ef95302288
Ping Xiao <xpusjobs@gmail.com>
2016-12-12 00:24:23 -0800

    update LRU Cache

--- src/design/LRUCache.java
+++ src/design/LRUCache.java
@@ -2,83 +2,81 @@ package design;
 import java.util.*;
 
 public class LRUCache {
-	private class DLinkedList{
-		int key, value;
-		DLinkedList prev, next;
-		
-		public DLinkedList(int key, int value){
-			this.key = key;
-			this.value = value;
-		}
-		
-		public DLinkedList(){
-			this.key = 0;
-			this.value = 0;
-		}
-	}
-	
-	private HashMap<Integer, DLinkedList> map;
-	private int capacity, count;
-	private DLinkedList head, tail;
-	
-	public LRUCache(int capacity) {
-		map = new HashMap<>();
-		this.capacity = capacity;
-		this.count = 0;		 
-		head = new DLinkedList();
-		tail = new DLinkedList();
-		head.next = tail;
-		tail.prev = head;
-	}
-	
-	public int get(int key){
-		DLinkedList node = map.get(key);
-		if(node == null){
-			return -1;
-		}else{
-			
-		}
-		return node.value;
-	}
-	
-	public void set(int key, int value){
-
-		DLinkedList node = map.get(key);
-		if(node == null){
-			if(count == capacity){
-				DLinkedList del = tail.prev;
-				remove(del);
-				map.remove(del.key);
-				count--;
-			}			
-			node = new DLinkedList(key, value);
-			add(node);
-			count++;
-		}else{
-			node.value = value;
-			update(node);			
-		}
-				
-	}
-	
-	public void add(DLinkedList node){
-		DLinkedList after = head.next;
-		head.next = node;
-		node.next = after;
-		node.prev = head;
-		after.prev = node;
-	}
-	
-	public void remove(DLinkedList node){
-		DLinkedList before = node.prev;
-		DLinkedList after = node.next;
-		before.next = after;
-		after.prev = before;
-	}
-	
-	public void update(DLinkedList node){
-		remove(node);
-		add(node);		
-	}
+	class DLinkedNode{
+        int key, value;
+        DLinkedNode prev, next;
+        
+        public DLinkedNode(int key, int value){
+            this.key = key;
+            this.value = value;
+        }
+        
+        public DLinkedNode(){
+            this.key = 0;
+            this.value = 0;
+        }
+    }
+    
+    HashMap<Integer, DLinkedNode> map;
+    DLinkedNode head, tail;
+    int capacity, count;
+    
+    public LRUCache(int capacity) {
+        map = new HashMap<>();
+        this.capacity = capacity;
+        this.count = 0;
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        DLinkedNode node = map.get(key);
+        if(node == null){
+            return -1;
+        }
+        update(node);
+        return node.value;
+    }
+    
+    public void set(int key, int value) {
+        DLinkedNode node = map.get(key);
+        if(node == null){
+            if(count == capacity){
+                DLinkedNode toDel = tail.prev;
+                remove(toDel);
+                map.remove(toDel.key);
+                count--;
+            }
+            DLinkedNode newNode = new DLinkedNode(key, value);
+            add(newNode);
+            map.put(key, newNode);
+            count++;
+        }else{
+            node.value = value;
+            update(node);
+        }
+    }
+    
+    public void add(DLinkedNode node){
+        DLinkedNode after = head.next;
+        node.next = head.next;
+        head.next = node;
+        node.prev = head;
+        after.prev = node;
+    }
+    
+    public void remove(DLinkedNode node){
+        DLinkedNode after = node.next;
+        DLinkedNode before = node.prev;
+        after.prev = before;
+        before.next = after;
+    }
+    
+    public void update(DLinkedNode node){
+        remove(node);
+        add(node);
+    }
 	
 }
