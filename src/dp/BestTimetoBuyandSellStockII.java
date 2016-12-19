6e91e8da99b07d1d14fca8afec2ceee012d325a8
Ping Xiao <xpusjobs@gmail.com>
2016-12-19 01:07:51 -0800

    Best Time to Buy and Sell Stock II

---
+++ src/dp/BestTimetoBuyandSellStockII.java
@@ -0,0 +1,16 @@
+package dp;
+
+public class BestTimetoBuyandSellStockII {
+	public int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 0) return 0;
+        
+        int max = 0;
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i] > prices[i - 1]){
+                max += prices[i] - prices[i - 1];
+            }
+        }
+        
+        return max;
+    }
+}
