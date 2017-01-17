25534efbaa941780cd54ac9b0e8334fe4745e1a8
Ping Xiao <xpusjobs@gmail.com>
2017-01-17 00:00:18 -0800

    Max Consecutive Ones II

--- src/array/MaxConsecutiveOnesII.java
+++ src/array/MaxConsecutiveOnesII.java
@@ -19,4 +19,17 @@ public class MaxConsecutiveOnesII {
 		}
 		return max;
 	}
+	
+	public int findMaxConsecutiveOnesII(int[] nums) {
+        int max = 0, zero = 0, k = 1; // flip at most k zero
+        for (int l = 0, h = 0; h < nums.length; h++) {
+            if (nums[h] == 0)                                           
+                zero++;
+            while (zero > k)
+                if (nums[l++] == 0)
+                    zero--;                                     
+            max = Math.max(max, h - l + 1);
+        }                                                               
+        return max;
+    }
 }
